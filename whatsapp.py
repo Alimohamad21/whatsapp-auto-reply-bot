50bfa29cbb58f6ed24e92c33465e99d7519e7004
Ali Mohamed <alimohamadyoussef21@gmail.com>
2021-01-10 20:13:44 +0200

    app maintanenance/big fixes

--- whatsapp.py
+++ whatsapp.py
@@ -9,16 +9,25 @@ driver.get('https://web.whatsapp.com/')  # opens whatsapp in the browser
 input("Press any key to continue")  # do this after scanning qr code with your phone
 
 
-def sing():
+def file_write(contact_name, received_message, sent_message):
+    f = open("replies.txt", "a")
+    t = datetime.datetime.now()
+    f.write("replied to <" + contact_name + ">'s: <" + received_message
+            + "> " + "with:<" + sent_message + "> " + " at <" + t.strftime(
+        "%Y-%m-%d %H:%M:%S>") + "\n")
+    f.close()
+
+
+def spam():
     name = input("contact:")
     contact = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
     contact.click()
-    msgBox = driver.find_elements(By.CLASS_NAME, "_3FRCZ")[1]
-    song = """"""
+    msgBox = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]')
+    song = """sh sh sh sh sh sh sh"""
     song = song.split()
     for i in range(len(song)):
         msgBox.send_keys(song[i])
-        sendButton = driver.find_element_by_class_name("_1U1xa")
+        sendButton = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]')
         sendButton.click()
 
 
@@ -28,9 +37,9 @@ def auto_reply():
     current = 0
     start_time = time.time()
     auto_reply_count = dict()  # counts how many times we replied to each chat
-    message_to_be_sent = "chuppa mi polla"
+    message_to_be_sent = "test"
     while time.time() - start_time < 28800:  # runs for 8 hours
-        contacts = driver.find_elements(By.CLASS_NAME, "_210SC")  # fetches last 15 chats(reads 15 max)
+        contacts = driver.find_elements(By.CLASS_NAME, "_1C6Zl")  # fetches last 15 chats(reads 15 max)
         try:
             contacts[current].click()  # tries to open the chat of current contact
         except:
@@ -38,15 +47,13 @@ def auto_reply():
         current += 1  # to watch all 15 chats
         if current == len(contacts):  # resets count so that it starts from first chat if it exceeds chat count
             current = 0
-        print("watching " + driver.find_element_by_class_name("_2FCjS").text + "...")
-        msgBox = driver.find_elements(By.CLASS_NAME, "_3FRCZ")[1]  # fetches the text box we type a message into
-        chat_messages = driver.find_elements(By.CLASS_NAME, "eRacY")  # fetches current chat messages
+        print("watching " + driver.find_element_by_class_name("YEe1t").text + "...")
+        msgBox = driver.find_element_by_xpath(
+            '/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]')  # fetches the text box we type a message into
+        chat_messages = driver.find_elements(By.CLASS_NAME, "_1wlJG")  # fetches current chat messages
         if driver.find_element_by_class_name(
-                "_2FCjS").text != contact_name:  # if you start watching a different chat,do this
-            contact_name = driver.find_element_by_class_name("_2FCjS").text  # update contact currently being watched
-            # if contact_name == "Squaad" or contact_name == "Nana" or contact_name == "Comrades" or contact_name == "Teta" or contact_name == "Valorant" or contact_name == "pokerona" or contact_name == "الريس عباري" or contact_name == "Tafheen CCE" or contact_name == "ux" or contact_name == "Mizo ds":
-            if not contact_name == "Abdelkarim Azp 3":
-                continue
+                "YEe1t").text != contact_name:  # if you start watching a different chat,do this
+            contact_name = driver.find_element_by_class_name("YEe1t").text  # update contact currently being watched
             if contact_name not in last_message.keys():  # if it's the first time we watch this chat,initialize it's info
                 if not len(chat_messages):  # avoid errors
                     continue
@@ -56,36 +63,29 @@ def auto_reply():
                     msgBox.send_keys(
                         message_to_be_sent)  # sends message we want to send to the text box we type a message into
                     last_message[contact_name] = message_to_be_sent  # updates current last message in chat
-                    sendButton = driver.find_element_by_class_name("_1U1xa")
-                    sendButton.click()  # clicks the send button for the message to be sent
-                    f = open("replies.txt", "a")
-                    t = datetime.datetime.now()
-                    f.write("replied to <" + contact_name + ">'s: <" + chat_messages[
-                        -1].text + "> " + "with:<" + message_to_be_sent + "> " + " at <" + t.strftime(
-                        "%Y-%m-%d %H:%M:%S>") + "\n")
-                    f.close()
+                    sendButton = driver.find_element_by_xpath(
+                        '/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]')
+                    if contact_name=="Test5" or contact_name=="Test4" or contact_name=="Test3" or contact_name=="Test2" or contact_name=="Test":
+                        sendButton.click()  # clicks the send button for the message to be sent
+                    file_write(contact_name, chat_messages[-1].text, message_to_be_sent)
                     continue
         print("All last messages:{}".format(last_message))  # prints last message of every watched chat
         print("{} last message:{}".format(contact_name,
                                           last_message[contact_name]))  # prints last message of currently watched chat
-        chat_messages = driver.find_elements(By.CLASS_NAME, "eRacY")
+        chat_messages = driver.find_elements(By.CLASS_NAME, "_1wlJG")
         print("comparing: " + chat_messages[-1].text + " and " + last_message[contact_name])
         if chat_messages[-1].text != last_message[
             contact_name]:  # if last stored message is different than the current last message in chat
             if auto_reply_count[contact_name] > 4:  # if you replied to this chat 5 times,don't reply again
                 continue
             msgBox.send_keys(message_to_be_sent)
-            sendButton = driver.find_element_by_class_name("_1U1xa")
-            sendButton.click()
+            sendButton = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]')
+            if contact_name == "Test5" or contact_name == "Test4" or contact_name == "Test3" or contact_name == "Test2" or contact_name == "Test":
+                sendButton.click()
             last_message[contact_name] = message_to_be_sent  # send message and update last message sent
             auto_reply_count[contact_name] += 1
-            f = open("replies.txt", "a")
-            t = datetime.datetime.now()
-            f.write("replied to <" + contact_name + ">'s: <" + chat_messages[
-                -1].text + "> " + "with:<" + message_to_be_sent + "> " + " at <" + t.strftime(
-                "%Y-%m-%d %H:%M:%S>") + "\n")
-            f.close()
+            file_write(contact_name, chat_messages[-1].text, message_to_be_sent)
 
 
 auto_reply()
-# sing()
+# spam()
